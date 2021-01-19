9a0b24c496036b61fd11f28f447e7f150fb8e07f
galka-travinskaya <galka03031988@rambler.ru>
2021-01-19 15:15:37 +0200

    hw

--- src/components/FriendsList/FriendList.js
+++ src/components/FriendsList/FriendList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import FriendListItem from './FriendListItem';
 
@@ -9,25 +9,22 @@ const FriendList = ({ friends }) => {
   if (friends.length === 0) return null;
   return (
     <ul 
-      className={s.friendList}>{friends.map(prop => <FriendListItem/>)}
+      className={s.friendList}>{friends.map(friend => <FriendListItem key={friend.id} friend={friend}/>)}
     </ul>
   )
     
 };
 
 // FriendList.propTypes = {
-//   friends: PropTypes.array,
+//   friends: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       isOnline: PropTypes.bool.isRequired,
+//       avatar: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//     })).isRequired,
 // };
 
-FriendList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-};
+
 
 export default FriendList;
--- src/components/FriendsList/FriendListItem.js
+++ src/components/FriendsList/FriendListItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import defaultImg from '../../images/default.jpg';
 
 import s from '../FriendsList/FriendListItem.module.css';
 
-const FriendListItem = ({ id, avatar, name = defaultImg, isOnline }) => {
+// const FriendListItem = ({ id, avatar, name, isOnline }) => {
+const FriendListItem = ({friend}) => { 
+  const avatar = friend.avatar;
+  const name = friend.name;
+  const isOnline = friend.isOnline;
   const StatusClassName = isOnline ? s.online : s.offLine;
   return (
     <li 
-      key={id} 
+      // key={id} 
       className={s.item}>
       <span 
         className={StatusClassName}>{}
@@ -24,13 +28,22 @@ const FriendListItem = ({ id, avatar, name = defaultImg, isOnline }) => {
   );
 };
 
+FriendListItem.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
 // FriendListItem.propTypes = {
 //   id: PropTypes.number.isRequired,
 //   avatar: PropTypes.string.isRequired,
 //   name: PropTypes.string.isRequired,
 //   isOnline: PropTypes.bool.isRequired,
 // };
-
-
 
 export default FriendListItem;
